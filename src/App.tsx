90101adabe39341f501f70e9294582907712cca9
dtnhan <dotrongnhan15102000@gmail.com>
2023-12-16 15:07:50 +0700

    fix: first commit

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { useCallback, useEffect, useState } from 'react';
+import './App.css';
+
+type UserProps = {
+    id: number;
+    email: string;
+    name: string;
+    phone?: string;
+    address?: string;
+    email_verified_at?: string;
+    created_at: string;
+    updated_at: string;
+};
+
+type ResponseProps = {
+    returnCnt: string;
+    users: UserProps[];
+    bizResult: string;
+};
+
+function App() {
+    const [users, setUsers] = useState<UserProps[]>([]);
+
+    const fetchApi = useCallback(async () => {
+        const response: ResponseProps = await axios
+            .get('http://localhost:8000/api/users')
+            .then((res) => res.data);
+
+        if (response.bizResult === '0') {
+            setUsers(response.users);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchApi();
+    }, [fetchApi]);
+
+    return (
+        <>
+            {users.map((value: UserProps, index: number) => {
+                return (
+                    <div key={index}>
+                        <ul>
+                            <li>{value.id}</li>
+                            <li>{value.email}</li>
+                            <li>{value.name}</li>
+                            <li>{value.phone}</li>
+                            <li>{value.address}</li>
+                            <li>{value.created_at}</li>
+                            <li>{value.updated_at}</li>
+                        </ul>
+                    </div>
+                );
+            })}
+        </>
+    );
+}
+
+export default App;
